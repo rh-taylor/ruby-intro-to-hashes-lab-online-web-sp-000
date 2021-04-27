1bd5e37700eb1e3afc2fc854a42baa6edac10dea
Robyn Taylor <rhtaylor0715@gmail.com>
2021-04-27 14:57:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,17 +2,18 @@ def new_hash
   new_hash = {}
 end
 
+
 def actor
   actor = {
     :name => "Dwayne The Rock Johnson"
   }
 end
 
+
 def monopoly
 	monopoly = {
 	  :railroads => {}
 	}
-
 end
 
 def monopoly_with_second_tier
